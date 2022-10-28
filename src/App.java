d8c95d7dd8c4ac2a8958829245f6a3a8abf84cad
bigodom <92757340+bigodom@users.noreply.github.com>
2022-10-28 20:44:49 -0300

    Add files via upload

--- src/App.java
+++ src/App.java
@@ -10,6 +10,14 @@ class App {
         arquivos[3] = "src/facebook_combined.txt";
         arquivos[4] = "src/USA-road-dt.DC.txt";
         arquivos[5] = "src/USA-road-dt.NY.txt";
+        
+        String mazes[] = new String[6];
+        mazes[0] = "files/maze3.txt";
+        mazes[1] = "files/maze10.txt";
+        mazes[2] = "files/maze20.txt";
+        mazes[3] = "files/maze30.txt";
+        mazes[4] = "files/maze40.txt";
+        mazes[5] = "files/maze50.txt";
 
         Scanner sc = new Scanner(System.in);
 
@@ -46,7 +54,7 @@ class App {
                  
 
                     switch(alg){
-                    	case 1:
+                        case 1:
 	                        GraphList g1 = new GraphList(arquivos[file]);
 		                    System.out.println();
 		                    System.out.println("[Dijkstra]");
@@ -56,7 +64,7 @@ class App {
 		                    float totalTime = System.currentTimeMillis() - startTime;
 		                    System.out.println("O tempo total foi de " + totalTime / 1000 + " segundos.");
 		                    break ;
-                    	case 2:
+                        case 2:
 		                    GraphList g3 = new GraphList(arquivos[file]);
 		                    System.out.println();
 		                    System.out.println("[Bellman Ford]");
@@ -86,22 +94,83 @@ class App {
 		                    totalTime = System.currentTimeMillis() - startTime;
 		                    System.out.println("O tempo total foi de " + totalTime / 1000 + " segundos.");
 		                    break;
-	                	}
-	                    break;
-	                case 2:
-	                    System.out.println("Informe o nome do arquivo:(Exemplo: files/maze/toy.txt)");
-	                    String filename = sc.next();
-	                    long startTimeLabirinto = System.currentTimeMillis();
-	                    //Labirinto labirinto = new Labirinto(filename);
-	                    float totalTimeLabirinto = System.currentTimeMillis() - startTimeLabirinto;
-	                    System.out.println("O tempo total foi de " + totalTimeLabirinto / 1000 + " segundos.");
+                   
+                    	}
+                    break;
+                case 2:
+                	System.out.println("digite o numero do arquivo que deseja: ");
+                	System.out.println("1 - maze3.txt ");
+                	System.out.println("2 - maze10.txt ");
+                	System.out.println("3 - maze20.txt ");
+                	System.out.println("4 - maze30.txt ");
+                	System.out.println("5 - maze40.txt ");
+                	System.out.println("6 - maze50.txt ");
+                	int lab = sc.nextInt();
+
+                	switch(lab) {
+                	case 1:
+
+	                    long startTimeMaze = System.currentTimeMillis();
+	                    Maze labirinto = new Maze(mazes[lab - 1]);
+	                    
+	                    float totalTimeMaze = System.currentTimeMillis() - startTimeMaze;
+	                    System.out.println("O tempo total foi de " + totalTimeMaze / 1000 + " segundos.");
 	
 	                    break;
-	                case 3:
-	                    break;
+                	case 2:
+
+	                    long startTimeMaze2 = System.currentTimeMillis();
+	                    Maze labirinto2 = new Maze(mazes[lab - 1]);
+	                    
+	                    float totalTimeMaze2 = System.currentTimeMillis() - startTimeMaze2;
+	                    System.out.println("O tempo total foi de " + totalTimeMaze2 / 1000 + " segundos.");
 	
-	                default:
-	                    System.out.println("Opcao invalida!");
+	                    break;
+                	case 3:
+
+	                    long startTimeMaze3 = System.currentTimeMillis();
+	                    Maze labirinto3 = new Maze(mazes[lab - 1]);
+	                    
+	                    float totalTimeMaze3 = System.currentTimeMillis() - startTimeMaze3;
+	                    System.out.println("O tempo total foi de " + totalTimeMaze3 / 1000 + " segundos.");
+	
+	                    break;
+                	case 4:
+
+	                    long startTimeMaze4 = System.currentTimeMillis();
+	                    Maze labirinto4 = new Maze(mazes[lab - 1]);
+	                    
+	                    float totalTimeMaze4 = System.currentTimeMillis() - startTimeMaze4;
+	                    System.out.println("O tempo total foi de " + totalTimeMaze4 / 1000 + " segundos.");
+	
+	                    break;
+                	case 5:
+
+	                    long startTimeMaze5 = System.currentTimeMillis();
+	                    Maze labirinto5 = new Maze(mazes[lab - 1]);
+	                    
+	                    float totalTimeMaze5 = System.currentTimeMillis() - startTimeMaze5;
+	                    System.out.println("O tempo total foi de " + totalTimeMaze5 / 1000 + " segundos.");
+	
+	                    break;
+                	case 6:
+
+	                    long startTimeMaze6 = System.currentTimeMillis();
+	                    Maze labirinto6 = new Maze(mazes[lab - 1]);
+	                    
+	                    float totalTimeMaze6 = System.currentTimeMillis() - startTimeMaze6;
+	                    System.out.println("O tempo total foi de " + totalTimeMaze6 / 1000 + " segundos.");
+	
+	                    break;
+                	}
+                	
+                    break;
+                case 3:
+                	System.out.println("Encerrando o programa!");
+                    break;
+
+                default:
+                    System.out.println("Opcao invalida!");
             }
 
         }
